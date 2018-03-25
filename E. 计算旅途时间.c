f30300eb07b9b88faa351b2f9fdaa9fb060f4704
yyhaos <35396510+yyhaos@users.noreply.github.com>
2018-03-25 22:43:02 +0800

    Delete E. 计算旅途时间.c

--- E. 计算旅途时间.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main ()
-{
-    int time_1,time_2;
-
-    scanf("%d%d",&time_1,&time_2);
-    printf("%d",-time_1/100%100*60-time_1%100+time_2/100%100*60+time_2%100);
-    return 0;
-}
